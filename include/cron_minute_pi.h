089d968bdfded00253948ff68a6cf527ce437bf7
Ivan Kuchin <ivan.kuchin@gmail.com>
2021-04-18 15:33:31 -0400

    initial commit

---
+++ include/cron_minute_pi.h
@@ -0,0 +1,20 @@
+#ifndef __CRON_MINUTE__H__
+#define __CRON_MINUTE__H__
+
+#include <sstream>
+#include <sys/time.h>
+#include <string.h>
+
+#include "cactivator.h"
+#include "ccgi.h"
+#include "cmysql.h"
+#include "cuser.h"
+#include "cmail.h"
+#include "cstatistics.h"
+#include "utilities.h"
+#include "localy.h"
+#include "clog.h"
+
+#define	ACTIVATOR_EMAIL_CHANGE						60	// --- minutes
+
+#endif
