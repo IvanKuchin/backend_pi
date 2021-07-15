b75565ad7b29de29eefbbade926269c7783fe4de
Ivan Kuchin <ivan.kuchin@gmail.com>
2021-07-15 13:57:42 -0400

    Infrastructure: change message severity

--- cstatistics.cpp
+++ cstatistics.cpp
@@ -13,7 +13,7 @@ CStatistics::~CStatistics()
 	endTime = time(NULL);
 	endChrono = std::chrono::duration_cast<std::chrono::microseconds> (system_clock::now().time_since_epoch());
 
-	MESSAGE_INFO ("", "", "time duration (in ticks) = " + to_string((float)(endClock - startClock) /  CLOCKS_PER_SEC));
 	MESSAGE_INFO ("", "", "chrono duration = " + to_string(endChrono.count() - startChrono.count()) + " microseconds");
+	MESSAGE_DEBUG("", "", "time duration (in ticks) = " + to_string((float)(endClock - startClock) /  CLOCKS_PER_SEC));
 	MESSAGE_DEBUG("", "", "sec duration = " + to_string(endTime - startTime));
 }
